e2158081d1c2df797489ae2b49f2eeebea333335
PriDev07 <lohaniprince72@gmail.com>
2025-04-17 08:25:16 +0530

    auth-state-persistent

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -8,6 +8,7 @@ import 'package:zoom_clone/utils/utils.dart';
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Stream<User?> get authChanges => _auth.authStateChanges();
   Future<bool> SignInWithGoogle(BuildContext context) async {
     bool res = false;
     try {
